98511831df0178865ac65356999802d3800bd2d5
Yevhen Malyshko <memx86@gmail.com>
2022-04-06 22:52:11 +0300

    unused proptypes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import s from "./Main.module.css";
 
 function Main(props) {
   return <div className={s.container}>Main</div>;
 }
 
-Main.propTypes = {};
+// Main.propTypes = {};
 
 export default Main;
