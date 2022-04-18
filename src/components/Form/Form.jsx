6718731cc63584d66f02dade162c3d8a632d3da7
Yevhen Malyshko <memx86@gmail.com>
2022-04-18 14:57:52 +0300

    form component hook refactoring

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,80 +1,76 @@
-import { Component } from "react";
+import { useMemo, useState } from "react";
 import PropTypes from "prop-types";
 import s from "./Form.module.css";
 import Button from "components/Button";
 
-class Form extends Component {
-  static propTypes = {
-    fields: PropTypes.arrayOf(
-      PropTypes.shape({
-        value: PropTypes.string,
-        ru: PropTypes.string,
-        en: PropTypes.string,
-      })
-    ).isRequired,
-    add: PropTypes.string,
-    handleSubmit: PropTypes.func.isRequired,
-  };
-  initialState = {};
-  state = {};
-  constructor({ fields }) {
-    super();
+function Form({ fields, add, handleSubmit }) {
+  const initialState = useMemo(() => {
+    const initialState = {};
     fields
       .map((field) => field.value)
       .forEach((e) => {
-        this.initialState[e] = "";
+        initialState[e] = "";
       });
-    this.state = { ...this.initialState };
-  }
-  onChange = ({ target }) => {
+    return initialState;
+  }, [fields]);
+  const [state, setState] = useState(initialState);
+
+  const onChange = ({ target }) => {
     const name = target.name;
     const value = target.value;
-    this.setState({ [name]: value });
+    setState((prevState) => ({ ...prevState, [name]: value }));
   };
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    this.props.handleSubmit({ ...this.state, options: "Some options" });
-    this.setState({ ...this.initialState });
+    handleSubmit({ ...state, options: "Some options" });
+    setState({ ...initialState });
   };
-  canSubmit = () => {
-    return Object.values(this.state).every((item) => !!item);
+  const canSubmit = () => {
+    return Object.values(state).every((item) => !!item);
   };
 
-  render() {
-    const { props, state, onSubmit, onChange, canSubmit } = this;
-    const { fields, add } = props;
-    return (
-      <form className={s.form} onSubmit={onSubmit}>
-        {fields.map(({ value, ru }) => {
-          let type;
-          switch (value) {
-            case "email":
-              type = "email";
-              break;
-            case "phone":
-              type = "tel";
-              break;
-            default:
-              type = "text";
-          }
-          return (
-            <label key={value} className={s.label}>
-              <input
-                className={s.input}
-                type={type}
-                name={value}
-                value={state[value]}
-                placeholder=" "
-                onChange={onChange}
-              />
-              <span className={s.text}>{ru}</span>
-            </label>
-          );
-        })}
-        <Button text={add} type="submit" disabled={!canSubmit()} />
-      </form>
-    );
-  }
+  return (
+    <form className={s.form} onSubmit={onSubmit}>
+      {fields.map(({ value, ru }) => {
+        let type;
+        switch (value) {
+          case "email":
+            type = "email";
+            break;
+          case "phone":
+            type = "tel";
+            break;
+          default:
+            type = "text";
+        }
+        return (
+          <label key={value} className={s.label}>
+            <input
+              className={s.input}
+              type={type}
+              name={value}
+              value={state[value]}
+              placeholder=" "
+              onChange={onChange}
+            />
+            <span className={s.text}>{ru}</span>
+          </label>
+        );
+      })}
+      <Button text={add} type="submit" disabled={!canSubmit()} />
+    </form>
+  );
 }
 
+Form.propTypes = {
+  fields: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.string,
+      ru: PropTypes.string,
+      en: PropTypes.string,
+    })
+  ).isRequired,
+  add: PropTypes.string,
+  handleSubmit: PropTypes.func.isRequired,
+};
 export default Form;
